ef43601b810719e647546c2c5f0e7489c697385a
poundsmichaelscode <michealolayenikan@gmail.com>
2026-06-10 22:38:26 +0100

    intro class

---
+++ intro.js
@@ -0,0 +1,9 @@
+const name = "Olaniyi";
+
+const age = 33;
+
+const favLanguage = "Javascript";
+
+
+console.log (`my name is   ${name} and  I am ${age} years old and my favorutre programming langugae is ${favLanguage}` );
+
