12b5d337012f82bccaf4e148f1681b2a43ce31f7
poundsmichaelscode <michealolayenikan@gmail.com>
2026-07-01 12:35:23 +0100

    variables in javascript

---
+++ array.js
@@ -0,0 +1,17 @@
+
+
+const fruits = ["mangoes", "Banana", "Aple", "Orang"];
+
+console.log (fruits.length)
+
+
+
+console.log ("pus methon to add somethin  at the end of the array ")
+
+
+
+fruits.push("penapple")
+
+
+
+console.log (fruits)
---
+++ variables.js
@@ -0,0 +1,8 @@
+
+
+
+var num = 5;
+
+let num = 10;
+
+const num = 10
